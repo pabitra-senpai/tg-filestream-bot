ccb2aa8756a2c8f2e10b40a083beaa5298f48746
Pabitra <pd7205020@gmail.com>
2026-07-31 22:31:58 +0530

    Update __init__.py

--- FileStream/server/__init__.py
+++ FileStream/server/__init__.py
@@ -1,14 +1,3 @@
-# ---------------------------------------------------
-# File Name: init.py
-# Author: NeonAnurag
-# GitHub: https://github.com/MyselfNeon/
-# Telegram: https://t.me/MyelfNeon
-# Created: 2025-11-21
-# Last Modified: 2025-11-22
-# Version: Latest
-# License: MIT License
-# ---------------------------------------------------
-
 from aiohttp import web
 from .stream_routes import routes
 
@@ -16,8 +5,3 @@ def web_server():
     web_app = web.Application(client_max_size=30000000)
     web_app.add_routes(routes)
     return web_app
-
-
-# MyselfNeon
-# Don't Remove Credit 🥺
-# Telegram Channel @NeonFiles
