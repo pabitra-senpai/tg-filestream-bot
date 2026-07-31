f5139ea6f7efce6a30d5e7d90f17bfe087d5145a
Pabitra <pd7205020@gmail.com>
2026-07-31 22:34:27 +0530

    Update clients.py

--- FileStream/bot/clients.py
+++ FileStream/bot/clients.py
@@ -1,14 +1,3 @@
-# ---------------------------------------------------
-# File Name: Clients.py
-# Author: NeonAnurag
-# GitHub: https://github.com/MyselfNeon/
-# Telegram: https://t.me/MyelfNeon
-# Created: 2025-11-21
-# Last Modified: 2025-11-22
-# Version: Latest
-# License: MIT License
-# ---------------------------------------------------
-
 import asyncio
 import logging
 from os import environ
@@ -68,8 +57,3 @@ async def initialize_clients():
         print("Multi-Client Mode Enabled")
     else:
         print("No additional clients were initialized, using default client")
-
-
-# MyselfNeon
-# Don't Remove Credit 🥺
-# Telegram Channel @NeonFiles
