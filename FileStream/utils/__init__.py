62d13d1ca3d047ca42cb17e2ea75faf586f279dd
Pabitra <pd7205020@gmail.com>
2026-07-31 22:17:49 +0530

    Update __init__.py

--- FileStream/utils/__init__.py
+++ FileStream/utils/__init__.py
@@ -1,19 +1,4 @@
-# ---------------------------------------------------
-# File Name: init.py
-# Author: NeonAnurag
-# GitHub: https://github.com/MyselfNeon/
-# Telegram: https://t.me/MyelfNeon
-# Created: 2025-11-21
-# Last Modified: 2025-11-22
-# Version: Latest
-# License: MIT License
-# ---------------------------------------------------
 
 from .time_format import get_readable_time
 from .file_properties import get_name, get_file_ids
 from .custom_dl import ByteStreamer
-
-
-# MyselfNeon
-# Don't Remove Credit 🥺
-# Telegram Channel @NeonFiles
